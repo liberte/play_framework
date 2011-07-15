806496de4fd035011b5ad113b3eeb022f89a732c
Mert Inan <morosophe_@windowslive.com>
2011-07-15 17:15:02 +0300

    yabe_test

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -1,13 +1,33 @@
 import org.junit.*;
-import java.util.*;
 import play.test.*;
 import models.*;
 
-public class BasicTest extends UnitTest {
+public class BasicTest extends UnitTest 
+{
 
-    @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
-    }
+@Test
+
+	public void tryConnectAsUser() 
+	{
+
+    new User("bob@gmail.com", "secret", "Bob").save();
+    
+   
+    assertNotNull(User.connect("bob@gmail.com", "secret"));
+    assertNull(User.connect("bob@gmail.com", "badpassword"));
+    assertNull(User.connect("tom@gmail.com", "secret"));
+	}
+	
+	public void CreateAndRetrieveUser()
+	{
+	
+	new User("bob@gmail.com","secret","Bob")._save();
+	User bob = User.find("byEmail", "bob@gmail.com").first();
+	
+	assertNotNull(bob);
+	assertNotNull("Bob",bob.fullname);
+	
+	
+	}
 
 }
